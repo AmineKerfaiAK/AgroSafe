d4243b41684295818ac2910c7376a4fd7a8b6206
FaresGuer <130791758+FaresGuer@users.noreply.github.com>
2025-11-19 18:01:25 +0100

    added buzzer

--- Projet_scenario_1/Core/Src/main.c
+++ Projet_scenario_1/Core/Src/main.c
@@ -45,8 +45,11 @@ typedef enum {
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
+TIM_HandleTypeDef htim1;
+
 /* USER CODE BEGIN PV */
 int status=0;
+int buzzer_on=0;
 ModeLuminosite currentMode = MODE_JOUR;
 /* USER CODE END PV */
 
@@ -54,6 +57,7 @@ ModeLuminosite currentMode = MODE_JOUR;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_TIM1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -71,7 +75,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
         {
             HAL_GPIO_WritePin(GPIOD, Green_Pin, GPIO_PIN_SET);
             HAL_GPIO_WritePin(GPIOD, Blue_Pin, GPIO_PIN_RESET);
-
+            HAL_GPIO_WritePin(GPIOD, Orange_Pin, GPIO_PIN_RESET);
             HD44780_Clear();
             HD44780_SetCursor(0,0);
             HD44780_PrintStr("SYSTEM");
@@ -82,7 +86,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
         {
             HAL_GPIO_WritePin(GPIOD, Green_Pin, GPIO_PIN_RESET);
             HAL_GPIO_WritePin(GPIOD, Blue_Pin, GPIO_PIN_RESET);
-
+            HAL_GPIO_WritePin(GPIOD, Orange_Pin, GPIO_PIN_RESET);
             HD44780_Clear();
             HD44780_SetCursor(0,0);
             HD44780_PrintStr("SYSTEM");
@@ -129,18 +133,20 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
         {
             // Soil is dry
             HAL_GPIO_WritePin(GPIOD, Orange_Pin, GPIO_PIN_SET);  // LED orange ON
+            HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
+            buzzer_on=1;
 
             HD44780_Clear();
             HD44780_SetCursor(0,0);
             HD44780_PrintStr("Sol sec -");
             HD44780_SetCursor(0,1);
-            HD44780_PrintStr("Irrigation");
+            HD44780_PrintStr("Pompe active");
         }
         else
         {
             // Soil is wet
+        	HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
             HAL_GPIO_WritePin(GPIOD, Orange_Pin, GPIO_PIN_RESET); // LED orange OFF
-
             HD44780_Clear();
             HD44780_SetCursor(0,0);
             HD44780_PrintStr("Sol humide");
@@ -184,6 +190,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
   HD44780_Init(2);
   HD44780_Clear();
@@ -191,6 +198,7 @@ int main(void)
   HD44780_PrintStr("Appuyer sur");
   HD44780_SetCursor(0,1);
   HD44780_PrintStr("le button");
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -198,7 +206,12 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  if(buzzer_on==1)
+	  {
+		  HAL_Delay(500);
+		  HAL_TIM_PWM_Stop(&htim1,TIM_CHANNEL_1);
+		  buzzer_on=0;
+	  }
     /* USER CODE BEGIN 3 */
 
 
@@ -286,6 +299,81 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 1000;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 500;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -300,6 +388,7 @@ static void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
